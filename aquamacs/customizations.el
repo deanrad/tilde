53015f60bb0594a0a3230d23b614889b063b60c2
dradcliffe <dradcliffe@backstopsolutions.com>
2010-06-10 10:49:00 -0500

    Moved aquamacs files in (and symlinked to them)

---
+++ aquamacs/customizations.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 208 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(default-frame-alist (quote ((fringe) (right-fringe) (left-fringe . 1) (internal-border-width . 0) (vertical-scroll-bars . right) (cursor-type . box) (menu-bar-lines . 1) (tool-bar-lines . 1) (background-color . "white") (background-mode . light) (border-color . "black") (cursor-color . "black") (foreground-color . "black") (mouse-color . "black"))))
+ '(grep-find-ignored-directories (quote ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" ".rsync_cache" "tmp" "log")))
+ '(iswitchb-mode t)
+ '(iswitchb-use-frame-buffer-list t)
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(server-mode t)
+ '(visual-line-mode nil t)
+ '(weblogger-config-alist (quote (("admin" "http://www.chicagogrooves.com/blog/xmlrpc.php" "admin" "" "1")))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
